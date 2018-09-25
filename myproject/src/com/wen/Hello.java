697ea98f5c44a55fb894b99e22ab3aeea4aeed5d
user <user@es302n26>
2018-09-25 11:58:50 +0800

    initial

---
+++ myproject/src/com/wen/Hello.java
@@ -0,0 +1,16 @@
+package com.wen;
+
+public class Hello {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = -1;
+		age = 19;
+		short num = 8;
+		float weight = (float)52.5;
+		System.out.println(age);
+		System.out.print(num);
+		System.out.println(weight);
+				
+	}
+
+}
